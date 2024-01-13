c5385eacb7e5f9d033b0c0ee47bcfa6fcf4cc797
thedanster7000 <danielson7000@gmail.com>
2024-01-13 17:45:55 +0000

    Added timer

---
+++ scripts/Timer.gd
@@ -0,0 +1,12 @@
+extends Label
+
+var time = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta
+	self.set_text(str(int(time)))
